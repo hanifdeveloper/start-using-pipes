a98a29cd7cbdfa2ac9e98b800d0dba1fd518ed60
zakuan <mzackwan90@gmail.com>
2019-01-05 11:04:15 +0700

    change format date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'usingpipes';
+  students = [
+    {
+      name: 'Junaidi',
+      gender: 'L',
+      birthdate: new Date(1990, 2, 16)
+    },
+    {
+      name: 'Jihan',
+      gender: 'L',
+      birthdate: new Date(1989, 3, 13)
+    },
+    {
+      name: 'Muhammad Zakuan',
+      gender: 'L',
+      birthdate: new Date(1990, 2, 1)
+    },
+    {
+      name: 'Siska',
+      gender: 'P',
+      birthdate: new Date(1992, 1, 4)
+    }
+  ];
 }
